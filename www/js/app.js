ac8070d25b3bebe9ef2f889caeb4109894e7b425
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-03 00:57:24 +0100

    removed ngRoute(obsolete) reference

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,4 @@
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var findParkApp = angular.module('findPark', ['ionic', 'firebase', 'findPark.controllers', 'ngRoute', 'uiGmapgoogle-maps'])
+var findParkApp = angular.module('findPark', ['ionic', 'firebase', 'findPark.controllers', 'uiGmapgoogle-maps'])
 
     .run(function($ionicPlatform, $rootScope, $firebase, $window, $ionicLoading, $firebaseSimpleLogin) {
         $ionicPlatform.ready(function() {
